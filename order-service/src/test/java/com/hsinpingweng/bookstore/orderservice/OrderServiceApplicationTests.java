29de3aece8d233a6e22c6611acf7e368e553487e
wenghsinping <ohmyhappy77@gmail.com>
2021-02-04 17:55:45 +0000

    First commit

---
+++ order-service/src/test/java/com/hsinpingweng/bookstore/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsinpingweng.bookstore.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/com/hsinpingweng/bookstore/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsinpingweng.bookstore.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
